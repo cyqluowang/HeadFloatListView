214991c48eb71f38be91f6e05193d0b40b2a37c1
cyqluowang <cyqluowang@gmail.com>
2013-09-30 16:27:04 +0800

    test

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@
 exports.index = function(req, res){
 
        function  say(world){
-           console.log("woshiyige tianai"+world)
+           console.log("woshiyige ---tianai"+world)
        }
 
     function excute(a,b){
